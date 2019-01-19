24987955e1edee8c4b545fff2299d1589be6b418
Sebastián Pinto-Agüero González <sebastian.pintoaguero@usach.cl>
2019-01-19 11:53:46 -0300

    Como antes pero mejor

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,26 @@
     </main>
 
     <footer>
-      Footer
+      <h2>Eventos</h2>
+      <div class="footer-details">
+          <h4>Entretenimiento</h4>
+
+          <h4>Social</h4>
+      </div>
+      <h2>Noticias</h2>
+      <div class="footer-details">
+
+          <h4>Comunidad Universitaria</h4>
+          <h4>Académico</h4>
+
+      </div>
+      <br> Portal de Eventos y Noticias USACH Móvil
+      <br>
+      <div class="footer-details">
+
+          Copyright 2018 CompuMundoHiperMegaRed. Todos los derechos reservados
+          <br> Portal Desarrollado por estudiantes para estudiantes
+      </div>
     </footer>
   </div>
 </template>
--- src/IndexNoLogin.vue
+++ src/IndexNoLogin.vue
@@ -13,20 +13,34 @@
         <div class="title title-container">Eventos</div>
           <div class="first">
             <div class="img-container">
-              <h2> Festival de Divulgación del Conocimiento "Cambalache"</h2>
+              <div class="text">Festival de Divulgación del Conocimiento "Cambalache"<br><br>12/12/2018 - 11:00</div>
             </div>
           </div>
           <div class="second">
-            <h2>¿Quieres descubrir eventos de acuerdo a tus intereses ? Ingresa a la plataforma y obten contenido personalizado</h2>
+            <h2>¿Quieres descubrir eventos de acuerdo a tus intereses?<br><br><a href="/login">Ingresa</a> a la plataforma y obten contenido personalizado</h2>
           </div>
-          <div class="item">Evento</div>
-          <div class="item">Evento</div>
+          <div class="item-1">
+            <div class="img-container"></div>
+            <div class="text">Charla: Modelos matemáticos para modelar colas y tiempos de espera: nuevas aproximaciones y análisis asintóticos<br><br>21/11/2018 - 10:00</div>
+          </div>
+          <div class="item-2">
+            <div class="img-container"></div>
+            <div class="text">Concierto de Navidad Orquesta Clásica y Coro U. de Santiago<br><br>01/12/2018 - 12:00</div>
+          </div>
         </div>
 
       <div class="news">
         <div class="title title-container">Noticias</div>
-        <div class="item">Noticia</div>
-        <div class="item">Noticia</div>
+        <div class="item-1">
+          <div class="img-container">
+            <div class="text">Consideran insuficiente medida que prohíbe la publicación de rankings Simce</div>
+          </div>
+        </div>
+        <div class="item-2">
+          <div class="img-container">
+            <div class="text">Radio U. de Santiago inicia programación 2018-2019 con propuestas de su audiencia</div>
+          </div>
+        </div>
       </div>
 
     </div>
