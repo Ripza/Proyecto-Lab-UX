d469f06894ae3075f4f67ebb7c0e73fc1cfd78f0
Lechofa <felipe.valenzuelaf@usach.cl>
2019-01-22 12:01:31 -0300

    Arreglos a footer

--- src/FooterContent.vue
+++ src/FooterContent.vue
@@ -1,22 +1,49 @@
 <template>
-<div>
-  
-  <h2>Eventos</h2>
-    <div class="footer-details">
-        <h4>Entretenimiento</h4>
-
-        <h4>Social</h4>
+<div style="display: grid;
+  			grid-template-columns: repeat(4, 25%);
+  			">
+	<div class="footer-details" style="font-weight: bold; grid-column: 1 / 2;">
+    <a href="#/events" class="login-button" style="font-weight: bold;" >
+    	<h2>Eventos</h2>
+	</a>
+        <a href="#/" class="login-button" style="font-weight: bold;">
+          <h4>Entretenimiento</h4>
+        </a>
+        <a href="#/" class="login-button" style="font-weight: bold;">
+       		<h4>Social</h4>
+    	</a>
     </div>
-    <h2>Noticias</h2>
-    <div class="footer-details">
-
-        <h4>Comunidad Universitaria</h4>
-        <h4>Académico</h4>
+    
+    <div class="footer-details" style="grid-column: 2 / 3;">
+    <a href="#/news" class="login-button" style="font-weight: bold;">
+    	<h2>Noticias</h2>
+    </a>
+        <a href="#/newscomunidad" class="login-button" style="font-weight: bold;">
+        	<h4>Comunidad Universitaria</h4>
+        </a>
+        <a href="#/newsacademico" class="login-button" style="font-weight: bold;">
+        	<h4>Académico</h4>
+    	</a>
+    </div>
+    	
+    <div class="footer-details" style='grid-column-start: 3; grid-column-end: 4'>
+    	<h2>El equipo</h2>
+    	<a href="#/AcerdaDe" class="login-button" style="font-weight: bold;">
+    		<h4>Acerca de</h4>
+    	</a>
+    	<a href="#/aboutUs" class="login-button" style="font-weight: bold;">
+    		<h4>Sobre Nosotros</h4>
+    	</a>
+    	<a href="#/Contacto" class="login-button" style="font-weight: bold;">
+    		<h4>Contacto</h4>
+    	</a>
 
     </div>
-    <br> Portal de Eventos y Noticias USACH Móvil
+    <br> 
     <br>
+
     <div class="footer-details">
+    	Portal de Eventos y Noticias USACH Móvil
         Copyright 2018 CompuMundoHiperMegaRed. Todos los derechos reservados
         <br> Portal Desarrollado por estudiantes para estudiantes
     </div>
@@ -36,7 +63,6 @@ export default{
     }
   },
   mounted:function(){
-    console.log('Index.vue')
   }
 }
 </script>
